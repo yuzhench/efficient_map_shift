51accc66ba261658a7888e226b4654e4f495ef4f
yuzhench <yuzhench@umich.edu>
2023-11-10 22:45:36 -0500

    Implement the single side link list

--- map_shift.cpp
+++ map_shift.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <algorithm> 
 #include <ctime>
+#include <chrono>
 
 using namespace std;
 
 //constant 
-const int length = 500;
-const int threshold = 250;
+const int length = 1000;
+const int threshold = 50;
+const int TOTAL_LENGTH = length*length;
 
 
 //struct
@@ -21,12 +23,12 @@ struct voxel{
     bool check; 
 };
 
-voxel new_volume[length*length*length];
-voxel volume[length*length*length];
+voxel new_volume[length*length];
+voxel volume[length*length];
 
 
-void array_copy (voxel volume[length*length*length], voxel new_volume[length*length*length]){
-    for (int i = 0; i < length*length*length; i++){
+void array_copy (voxel volume[length*length], voxel new_volume[length*length]){
+    for (int i = 0; i < length*length; i++){
         volume[i] = new_volume[i];
     }
 }
@@ -34,8 +36,8 @@ void array_copy (voxel volume[length*length*length], voxel new_volume[length*len
 //helper function 
 
 //initialize the new_volume 
-void initialize_array(voxel volume[length*length*length]){
-    for (int i = 0; i < length*length*length; i++){
+void initialize_array(voxel volume[length*length]){
+    for (int i = 0; i < length*length; i++){
         volume[i].number = 1;
         volume[i].check = true;
     }
@@ -46,9 +48,9 @@ void initialize_array(voxel volume[length*length*length]){
 void move_right (){
 
     int delete_old_start = 0;
-    int delete_old_end = length*length*(threshold);
+    int delete_old_end = length*(threshold);
     int keep_old_start = delete_old_end;
-    int keep_old_end = length*length*length;
+    int keep_old_end = length*length;
 
     int create_new_start = keep_old_end-keep_old_start;
     int create_new_end =  keep_old_end;
@@ -81,16 +83,175 @@ void move_right (){
 }
  
 
+class voxel_node{
+public:
+    int number;
+    bool check; 
+    voxel_node* next; 
+
+    //constructor 
+    voxel_node(int number_in, bool check_in) : number(number_in), check(check_in),next(nullptr){}
+
+};
+
+class map_2D {
+public:
+    voxel_node* head;
+    voxel_node* tail;
+
+    void append(int number_in, int check_in) {
+        voxel_node* newNode = new voxel_node(number_in, check_in);
+        if (head == nullptr) {
+            // If the list is empty, make the new node the head
+            head = newNode;
+        } else {
+            // Traverse the list to find the last node
+            voxel_node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            // Add the new node at the end
+            temp->next = newNode;
+        }
+    }
+
+
+    pair<voxel_node*, voxel_node*> initialize_a_range(int number_array [],bool check_array [], int size){
+        pair<voxel_node*, voxel_node*> head_tail; 
+        head_tail.first = new voxel_node(number_array[0], check_array[0]);
+        head_tail.second = new voxel_node(number_array[size-1], check_array[size-1]);
+        voxel_node* current_node = head_tail.first;
+        for (int i = 1; i < size-1; i++){
+            voxel_node* temp_node = new voxel_node(number_array[i], check_array[i]);
+            current_node->next = temp_node;
+            current_node = temp_node;
+        }
+        current_node->next = head_tail.second;
+        return head_tail; 
+    }
+   
+    void delete_one_row(voxel_node* start_pointer){
+        voxel_node* current = start_pointer;
+        voxel_node* next_node; 
+        for (int i = 0; i < length; i++){
+            next_node = current->next;
+            delete current;
+            current = next_node;
+        }
+    }
+
+ 
+
+    voxel_node* get_new_head(){
+        voxel_node* new_head = head;
+
+        for (int i = 0; i < length; i++){
+            new_head = new_head->next;
+        }
+        return new_head;
+    }
+
+    void map_move_to_south(int new_number_array [],bool new_check_array [],int size){ 
+        voxel_node* old_head = head;
+        head = get_new_head();
+        delete_one_row(old_head);
+        pair<voxel_node*, voxel_node*> head_tail;
+        head_tail = initialize_a_range(new_number_array,new_check_array,length);
+        //store the address of the old tail address
+        voxel_node* old_tail = tail;
+        //upddate the new tail 
+        tail=head_tail.second;
+        //connect the new row to the old map
+        old_tail->next = head_tail.first;
+    }
+
+    // Display the linked list in a table format
+    void displayTable(int numRows, int numCols) {
+        voxel_node* temp = head;
+
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j < numCols; ++j) {
+                if (temp != nullptr) {
+                    std::cout << temp->number << "\t";
+                    temp = temp->next;
+                } else {
+                    // If the linked list is shorter than expected, print a placeholder
+                    std::cout << "N/A\t";
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+};
 
 
 
 int main(){
-    clock_t start, end;
+ 
 
-    initialize_array(volume);
-    start = clock();
-    move_right();
-    end = clock();
-    cout << "the time is: " << end - start << endl; 
+    // // initialize_array(volume);
+
+
+    // auto start = std::chrono::high_resolution_clock::now();
+    // // move_right();
+    // initialize_array(volume);
+    // auto stop = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    // cout << "the time is: " << duration.count() << " milliseconds." << std::endl;
+
+
+
+
+
+
+
+
+    cout<< "hollow world" << endl;
+
+    //create an example array 
+    int number [TOTAL_LENGTH]; 
+    bool check [TOTAL_LENGTH];
+    for (int i = 0; i < TOTAL_LENGTH; i++){
+        number[i] = i;
+        check [i] = true;
+    }
+
+    //create the new row 
+    int new_number [length];
+    bool new_chceck [length];
+    for (int i = 0; i < length; i++){
+        new_number[i] = 0;
+        new_chceck[i] = false;
+    }
+
+    // cout<< "hollow world" << endl;
+
+
+
+    // //link_list_method 
+    // map_2D Map;
+    // auto start = std::chrono::high_resolution_clock::now();
+    // pair<voxel_node*, voxel_node*> head_tail = Map.initialize_a_range(number,check,TOTAL_LENGTH);
+    // auto stop = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    // cout << "the time used to initialize the mao is: " << duration.count() << " milliseconds." << endl;
+
+    // Map.head = head_tail.first;
+    // Map.tail = head_tail.second;
+    // // cout << "---> " << Map.tail->number << endl;
+    // // Map.displayTable(length, length);
+
+    // auto start1 = std::chrono::high_resolution_clock::now();
+    // Map.map_move_to_south(new_number,new_chceck, length);
+    // auto stop1 = std::chrono::high_resolution_clock::now();
+    // auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(stop1 - start1);
+    // cout << "the time used update the new array is: " << duration1.count() << " milliseconds." << endl;
+
+    // // cout << endl << endl << endl;
+
+    // // Map.displayTable(length, length);
+
+
     return 0 ;
 }
